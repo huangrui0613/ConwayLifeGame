f5f754f47300138f26917c0407a2c34c22b500b5
huangrui <3105038768@qq.com>
2024-05-29 19:32:01 +0800

    修改 全屏显示界面

--- app/src/main/java/com/huangrui/conwaylifegame/ConwayLifeGameView.java
+++ app/src/main/java/com/huangrui/conwaylifegame/ConwayLifeGameView.java
@@ -19,33 +19,37 @@ public class ConwayLifeGameView extends View {
 
     private static final String TAG = "ConwayLifeGameView";
 
-    // 行列数
-    private final static int LINES = 100;
+    // 列数
+    private static final int COLS = 150;
+    // 行数
+    private int rows;
+    // 距离底部
+    private static final int MARGIN_BOTTOM = 0;
     // 细胞总数
     private int total;
 
     // 线条画笔
     private Paint linePaint;
     // 线条宽度
-    private final static int EDGE = 2;
+    private static final int EDGE = 2;
 
     // 细胞画笔
     private Paint cellPaint;
     // 细胞宽度
     private float size;
     // 细胞状态
-    private Set<Integer> cellSet;
+    private Set<Integer> cellSet = new HashSet<>();
 
     // 踪迹画笔
     private Paint tracePaint;
     // 踪迹
-    private Set<Integer> traceSet;
+    private final Set<Integer> traceSet = new HashSet<>();
 
     // 第几代细胞
-    private int generation;
+    private int generation = 1;
 
-    // 保存最近3代细胞数据
-    private Map<Integer, Set<Integer>> map;
+    // 记录最近3代所有细胞状态
+    private Map<Integer, Set<Integer>> recordMap;
 
     public ConwayLifeGameView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -54,55 +58,47 @@ public class ConwayLifeGameView extends View {
 
     private void init() {
         initPaint();
-        initLife();
     }
 
     private void initPaint() {
         linePaint = new Paint();
         linePaint.setStyle(Paint.Style.FILL);
-        linePaint.setColor(ContextCompat.getColor(getContext(), R.color.grey));
+        linePaint.setColor(ContextCompat.getColor(getContext(), R.color.color_line));
 
         cellPaint = new Paint();
         cellPaint.setStyle(Paint.Style.FILL);
-        cellPaint.setColor(ContextCompat.getColor(getContext(), R.color.purple_700));
+        cellPaint.setColor(ContextCompat.getColor(getContext(), R.color.purple_500));
 
         tracePaint = new Paint();
         tracePaint.setStyle(Paint.Style.FILL);
-        tracePaint.setColor(ContextCompat.getColor(getContext(), R.color.teal_200));
-    }
-
-    private void initLife() {
-        total = LINES * LINES;
-
-        initCell();
+        tracePaint.setColor(ContextCompat.getColor(getContext(), R.color.color_trace));
     }
 
     private void initCell() {
-        cellSet = new HashSet<>();
-        traceSet = new HashSet<>();
-        generation = 1;
+        size = (getWidth() - (COLS + 1) * EDGE) * 1.0f / COLS;
+        rows = (int) ((getHeight() - MARGIN_BOTTOM) / (size + EDGE));
+        total = COLS * rows;
 
-        map = new LinkedHashMap<Integer, Set<Integer>>(){
+        recordMap = new LinkedHashMap<Integer, Set<Integer>>(){
             @Override
             protected boolean removeEldestEntry(Entry eldest) {
                 return this.size() > 3;
             }
         };
 
-        Random random = new Random();
         Set<Integer> cells = new HashSet<>();
+        Random random = new Random();
         while (true) {
-            int index = random.nextInt(total);
-            cells.add(index);
-
-            // 随机生成细胞总数的1/5
+            cells.add(random.nextInt(total));
             if (cells.size() == total / 5) {
                 break;
             }
         }
         for (Integer cell : cells) {
-            addCell(cell % LINES, cell / LINES);
+            addCell(cell % COLS, cell / COLS);
         }
+
+        recordMap.put(generation, cellSet);
     }
 
     private float getXY(int line) {
@@ -111,7 +107,7 @@ public class ConwayLifeGameView extends View {
 
     // 添加细胞
     private void addCell(int col, int row) {
-        cellSet.add(row * LINES + col);
+        cellSet.add(row * COLS + col);
         // 添加踪迹
         addTrace(col, row);
     }
@@ -119,8 +115,8 @@ public class ConwayLifeGameView extends View {
     // 画出细胞
     private void drawCell(Canvas canvas) {
         for (Integer integer : cellSet) {
-            float x = getXY(integer % LINES);
-            float y = getXY(integer / LINES);
+            float x = getXY(integer % COLS);
+            float y = getXY(integer / COLS);
 
             canvas.drawRect(x, y, x + size, y + size, cellPaint);
         }
@@ -128,14 +124,14 @@ public class ConwayLifeGameView extends View {
 
     // 添加踪迹
     private void addTrace(int col, int row) {
-        traceSet.add(row * LINES + col);
+        traceSet.add(row * COLS + col);
     }
 
     // 画出历史踪迹
     private void drawTrace(Canvas canvas) {
         for (Integer integer : traceSet) {
-            float x = getXY(integer % LINES);
-            float y = getXY(integer / LINES);
+            float x = getXY(integer % COLS);
+            float y = getXY(integer / COLS);
 
             canvas.drawRect(x, y, x + size, y + size, tracePaint);
         }
@@ -154,6 +150,8 @@ public class ConwayLifeGameView extends View {
         cellSet.addAll(tempCellSet);
         // 记录轨迹
         traceSet.addAll(tempCellSet);
+
+        recordMap.put(generation, cellSet);
     }
 
     // 单个细胞下一代状态是否存活 true存活 false死亡
@@ -173,8 +171,8 @@ public class ConwayLifeGameView extends View {
 
     // 获取邻居数量
     private int getNeighbor(int index) {
-        int col = index % LINES;
-        int row = index / LINES;
+        int col = index % COLS;
+        int row = index / COLS;
 
         int neighbor = 0;
         for (int i = col - 1; i <= col + 1; i++) {
@@ -183,7 +181,7 @@ public class ConwayLifeGameView extends View {
                     continue;
                 }
 
-                if (isAlive(getLoop(j) * LINES + getLoop(i))) {
+                if (isAlive(getLoop(j, false) * COLS + getLoop(i, true))) {
                     neighbor++;
                 }
             }
@@ -192,14 +190,25 @@ public class ConwayLifeGameView extends View {
         return neighbor;
     }
 
-    // 当数值为边缘数值时 返回轮回数值
-    private int getLoop(int val) {
-        if (val == -1) {
-            return LINES - 1;
-        }
+    // 顶部和底部相连 左边和右边相连 当数值为边缘数值时 返回轮回数值
+    private int getLoop(int val, boolean isCol) {
 
-        if (val == LINES) {
-            return 0;
+        if (isCol) {
+            if (val == -1) {
+                return COLS - 1;
+            }
+
+            if (val == COLS) {
+                return 0;
+            }
+        } else {
+            if (val == -1) {
+                return rows - 1;
+            }
+
+            if (val == rows) {
+                return 0;
+            }
         }
 
         return val;
@@ -211,54 +220,57 @@ public class ConwayLifeGameView extends View {
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
-    }
-
-    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int width = getWidth();
-        size = (width - (LINES + 1) * EDGE) * 1.0f / LINES;
-
-        float distance;
-        for (int i = 0; i <= LINES; i++) {
-            distance = i * (size + EDGE);
-            canvas.drawRect(0, distance, width, distance + EDGE, linePaint);
-            canvas.drawRect(distance, 0, distance + EDGE, width, linePaint);
-        }
-
-        Log.e(TAG, String.format("第%d代细胞", generation));
-        map.put(generation, cellSet);
-        Set<Integer> generationSet = map.keySet();
-
-        boolean over = false; // 结束繁衍
-        for (Integer integer : generationSet) {
-            // 第一与第二代 或者第一与第三代相同则停止
-            boolean firEqSec = cellEquals(map.get(integer), map.get(integer + 1));
-            boolean firEqThr = cellEquals(map.get(integer), map.get(integer + 2));
-            if (firEqSec || firEqThr) {
-                over = true;
-                break;
-            }
-        }
-
-        if (!over) {
-            drawTrace(canvas);
-            drawCell(canvas);
-            totalNext();
-            generation++;
-        } else {
-            // 重启生命
-            Log.e(TAG, "重启生命");
+        if (cellSet.size() == 0) {
             initCell();
         }
 
-        postDelayed(() -> invalidate(), 100);
+        float distance;
+        // 画横线
+        for (int i = 0; i <= rows; i++) {
+            distance = i * (size + EDGE);
+            canvas.drawRect(0, distance, getWidth(), distance + EDGE, linePaint);
+        }
+
+        float height = rows * (size + EDGE);
+        // 画竖线
+        for (int i = 0; i <= COLS; i++) {
+            distance = i * (size + EDGE);
+            canvas.drawRect(distance, 0, distance + EDGE, height, linePaint);
+        }
+
+        Log.e(TAG, String.format("第%d代细胞", generation));
+        drawTrace(canvas);
+        drawCell(canvas);
+
+        final Set<Integer> recordSet = recordMap.keySet();
+        int genera = recordSet.iterator().next();
+        if (equals(recordMap.get(genera), recordMap.get(genera + 1)) || equals(recordMap.get(genera), recordMap.get(genera + 2))) {
+            // 如果当代生命与下一代相同 或者当代生命与隔代相同 此时界面类似一潭死水 2秒后重新绘制新的细胞
+            postDelayed(() -> {
+                generation = 1;
+                traceSet.clear();
+                cellSet.clear();
+                Log.e(TAG, "重生");
+                invalidate();
+            }, 2000);
+        } else {
+            totalNext();
+            generation++;
+            invalidate();
+        }
     }
 
-    private boolean cellEquals(Set<Integer> set1, Set<Integer> set2) {
+    @Override
+    public boolean isHardwareAccelerated() {
+        // 开启硬件加速
+        return true;
+    }
+
+    // 对比两代生命是否相同
+    private boolean equals(Set<Integer> set1, Set<Integer> set2) {
         if (set1 == null || set2 == null) {
             return false;
         }
